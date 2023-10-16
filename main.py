37b8d4cada52a0e6aaea05ea1bc0131c057fd90e
Jeffrey Scott <valenpendragon@gmail.com>
2023-10-16 10:32:18 -0400

    Add code to generate a complete reports on all files found in target_dir. Comment out tracing print statements.

--- main.py
+++ main.py
@@ -42,13 +42,13 @@ def check_zip(filepath):
             zip_file = zipfile.ZipFile(filepath)
             ret = zip_file.testzip()
             if ret is not None:
-                print(f"The first bad file in {filepath} is {ret}")
+                # print(f"The first bad file in {filepath} is {ret}")
                 result = (False, {ret})
             else:
                 result = (True, None)
             return result
         except Exception as e:
-            print(f"Opening zip file, {filepath}, produced Exception: {e}")
+            # print(f"Opening zip file, {filepath}, produced Exception: {e}")
             result = (False, e)
             return result
 
@@ -76,7 +76,7 @@ if __name__ == "__main__":
     args = sys.argv
     len_args = len(args)
     valid_arguments = ['-p', '--pdf', '-z', '--zip']
-    print(f"args: {args}")
+    # print(f"args: {args}")
 
     help_txt = "Usage: main.py [-p|--pdf] [-z|--zip] directory"
 
@@ -96,7 +96,7 @@ if __name__ == "__main__":
     # Identify target directory and remove it from arguments.
     target_dir = args[-1]
     args.pop(-1)
-    print(f"target_dir: {target_dir}")
+    # print(f"target_dir: {target_dir}")
 
     # Setting up file types to examine.
     file_types = []
@@ -120,7 +120,7 @@ if __name__ == "__main__":
 
     # Remove duplicate file types.
     file_types = list(set(file_types))
-    print(f"file_types: {file_types}")
+    # print(f"file_types: {file_types}")
 
     # Check target directory to see if it exists and is readable.
     if not os.path.exists(target_dir):
@@ -131,4 +131,30 @@ if __name__ == "__main__":
         exit(2)
 
     file_list = get_files_from_target_dir(target_dir, file_types)
-    print(f"file_list: {file_list}")
+    # print(f"file_list: {file_list}")
+
+    # Testing the files. A list of tuples is created for the output.
+    test_results = []
+    for file in file_list:
+        # PurePath often adds a leading period. So, we need to eliminate that from
+        # the suffix before using it.
+        fp = pathlib.PurePath(file)
+        suffix = pathlib.PurePath(file).suffix.replace(".", "")
+        # print(f"fp: {fp}. suffix: {suffix}.")
+        match suffix:
+            case 'pdf':
+                result = check_pdf(file)
+            case 'zip':
+                result = check_zip(file)
+        test_results.append((file, result[0], result[1]))
+
+    # Printing results to stdout.
+    list_file_types = [f" {file_type}" for file_type in file_types]
+    print(f"corrupt_file_finder: Here are the findings for supported file types,{list_file_types}:")
+    for file, result, error_msg in test_results:
+        if result:
+            print(f"{file}\tusable\tNo errors found")
+        else:
+            print(f"{file}\tcorrupted\t{error_msg}")
+    print("corrupt_file_finder: Report completed.")
+
